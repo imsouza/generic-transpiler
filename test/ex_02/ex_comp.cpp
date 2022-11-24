53ff8f5c54dbd574e89c99a44c33e4132b28fcbc
joilnen <joilnen@gmail.com>
2022-11-24 07:38:04 -0300

    suportando "if" dentro de "if" mas "for" dentro de "for" da pau, alguem da uma luz aih

--- test/ex_02/ex_comp.cpp
+++ test/ex_02/ex_comp.cpp
@@ -1,6 +1,7 @@
 /**
  * Aluno: joilnen leite
  */
+#define BOOST_SPIRIT_DEBUG
 #include <boost/spirit/include/qi.hpp>
 #include <string>
 #include <vector>
@@ -13,35 +14,43 @@ using namespace std;
 using namespace boost::spirit;
 
 template <typename Iterator, typename Skipper>
-struct pytoc_grammar : qi::grammar<Iterator,
-  std::vector<std::string>(), Skipper> {
-    pytoc_grammar() : pytoc_grammar::base_type{codigo} {
-        variavel_inteira = lexeme[qi::char_ >> *qi::alnum];
-        atribuicao =  variavel_inteira >> lit('=') >> qi::int_ >> lit(';');
-        for_instrucao = qi::string("for") >> '(' >> range_expressao >> ')' >> '{' >> *atribuicao || *if_instrucao >> '}';
-        range_expressao = variavel_inteira >> qi::string("in") >> qi::string("range") >> '('  >> qi::int_ >> ')';
-        if_instrucao = qi::string("if") >> '(' >> comp_expressao >> ')' >> '{' >> *atribuicao >> '}';
-        comp_expressao = (variavel_inteira >> '<' >> qi::int_) |
-                         (variavel_inteira >> '>' >> qi::int_) |
-                         (variavel_inteira >> qi::string("==") >> qi::int_);
-        codigo = *atribuicao || *for_instrucao || *if_instrucao;
-    }
+class PyToCpp : public qi::grammar<Iterator, std::vector<std::string>(), Skipper> {
 
-    qi::rule<Iterator, std::string(), Skipper> variavel_inteira;
-    qi::rule<Iterator, std::string(), Skipper> atribuicao;
-    qi::rule<Iterator, std::string(), Skipper> for_instrucao;
-    qi::rule<Iterator, std::string(), Skipper> if_instrucao;
-    qi::rule<Iterator, std::string(), Skipper> comp_expressao;
-    qi::rule<Iterator, std::string(), Skipper> range_expressao;
-    qi::rule<Iterator, std::string(), Skipper> print_funcao;
-    qi::rule<Iterator, std::vector<std::string>(), Skipper> codigo;
+    qi::rule<Iterator, std::string(), Skipper> VARIAVEL_INTEIRA;
+    qi::rule<Iterator, std::string(), Skipper> ATRIBUICAO;
+    qi::rule<Iterator, std::string(), Skipper> FOR_INSTRUCAO;
+    qi::rule<Iterator, std::string(), Skipper> FOR_INSTRUCAO_REC;
+    qi::rule<Iterator, std::string(), Skipper> IF_INSTRUCAO;
+    qi::rule<Iterator, std::string(), Skipper> IF_INSTRUCAO_REC;
+    qi::rule<Iterator, std::string(), Skipper> COMP_EXPRESSAO;
+    qi::rule<Iterator, std::string(), Skipper> RANGE_EXPRESSAO;
+    qi::rule<Iterator, std::string(), Skipper> PRINT_FUNCAO;
+    qi::rule<Iterator, std::vector<std::string>(), Skipper> CODIGO;
+
+    public:
+
+        PyToCpp() : PyToCpp::base_type{CODIGO} {
+
+            VARIAVEL_INTEIRA = lexeme[qi::char_ >> *qi::alnum];
+            ATRIBUICAO =       VARIAVEL_INTEIRA >> lit('=') >> qi::int_ >> lit(';');
+            FOR_INSTRUCAO_REC =    qi::string("for") >> '(' >> RANGE_EXPRESSAO >> ')' >> '{' >> *ATRIBUICAO >> '}';
+            FOR_INSTRUCAO =    qi::string("for") >> '(' >> RANGE_EXPRESSAO >> ')' >> '{' >> *ATRIBUICAO >> '}';
+            RANGE_EXPRESSAO = VARIAVEL_INTEIRA >> qi::string("in") >> qi::string("range") >> '('  >> qi::int_ >> ')';
+            IF_INSTRUCAO_REC = qi::string("if") >> '(' >> COMP_EXPRESSAO >> ')' >> '{' >> *ATRIBUICAO >> '}';
+            IF_INSTRUCAO =     qi::string("if") >> '(' >> COMP_EXPRESSAO >> ')' >> '{' >> *IF_INSTRUCAO_REC || *ATRIBUICAO >> '}';
+            COMP_EXPRESSAO = (VARIAVEL_INTEIRA >> '<' >> qi::int_) |
+                             (VARIAVEL_INTEIRA >> '>' >> qi::int_) |
+                             (VARIAVEL_INTEIRA >> qi::string("==") >> 
+                              qi::int_);
+            CODIGO =        *ATRIBUICAO || *FOR_INSTRUCAO || *IF_INSTRUCAO;
+        }
 };
 
 int main()
 {
     for (std::string s; std::getline(std::cin, s);) {
         auto it = s.begin();
-        pytoc_grammar<std::string::iterator, ascii::space_type> g;
+        PyToCpp<std::string::iterator, ascii::space_type> g;
         std::vector<std::string> v;
         if (qi::phrase_parse(it, s.end(), g, ascii::space, v)) {
             for (const auto& elem : v)
