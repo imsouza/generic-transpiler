5d5acd6525b68adc800934f8a8142d17ae7c2a0b
joilnen <joilnen@gmail.com>
2022-11-25 06:57:46 -0300

    ainda vendo os casos de for dentro de for

--- test/ex_02/ex_comp.cpp
+++ test/ex_02/ex_comp.cpp
@@ -33,16 +33,16 @@ class PyToCpp : public qi::grammar<Iterator, std::vector<std::string>(), Skipper
 
             VARIAVEL_INTEIRA = lexeme[qi::char_ >> *qi::alnum];
             ATRIBUICAO =       VARIAVEL_INTEIRA >> lit('=') >> qi::int_ >> lit(';');
-            FOR_INSTRUCAO_REC =    qi::string("for") >> '(' >> RANGE_EXPRESSAO >> ')' >> '{' >> *ATRIBUICAO >> '}';
-            FOR_INSTRUCAO =    qi::string("for") >> '(' >> RANGE_EXPRESSAO >> ')' >> '{' >> *ATRIBUICAO >> '}';
-            RANGE_EXPRESSAO = VARIAVEL_INTEIRA >> qi::string("in") >> qi::string("range") >> '('  >> qi::int_ >> ')';
+            FOR_INSTRUCAO_REC = qi::string("for") >> '(' >> RANGE_EXPRESSAO >> ')' >> '{' >> *ATRIBUICAO >> '}';
+            FOR_INSTRUCAO =     qi::string("for") >> '(' >> RANGE_EXPRESSAO >> ')' >> '{' >> *FOR_INSTRUCAO_REC ^ *ATRIBUICAO >> '}';
+            RANGE_EXPRESSAO =  VARIAVEL_INTEIRA >> qi::string("in") >> qi::string("range") >> '('  >> qi::int_ >> ')';
             IF_INSTRUCAO_REC = qi::string("if") >> '(' >> COMP_EXPRESSAO >> ')' >> '{' >> *ATRIBUICAO >> '}';
             IF_INSTRUCAO =     qi::string("if") >> '(' >> COMP_EXPRESSAO >> ')' >> '{' >> *IF_INSTRUCAO_REC || *ATRIBUICAO >> '}';
             COMP_EXPRESSAO = (VARIAVEL_INTEIRA >> '<' >> qi::int_) |
                              (VARIAVEL_INTEIRA >> '>' >> qi::int_) |
                              (VARIAVEL_INTEIRA >> qi::string("==") >> 
                               qi::int_);
-            CODIGO =        *ATRIBUICAO || *FOR_INSTRUCAO || *IF_INSTRUCAO;
+            CODIGO =         *ATRIBUICAO || *FOR_INSTRUCAO || *IF_INSTRUCAO;
         }
 };
 
