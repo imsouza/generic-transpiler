6e79e194bda610adf8cf73409409d5107de870fb
joilnen <joilnen@gmail.com>
2022-12-03 13:02:08 -0300

    gerando o primeiro codigo completo

--- test/alpha/ex_comp.cpp
+++ test/alpha/ex_comp.cpp
@@ -21,10 +21,6 @@ using phoenix::ref;
 
 std::vector<boost::variant<int, std::string, bool>> traduzido;
 
-void addToOutput(boost::variant<int, std::string, bool> &t) {
-    traduzido.emplace_back(t);
-}
-
 void imprimeFor(std::string &a) {
     cout << "FOR: " << a << "\n"; 
 }
@@ -57,7 +53,12 @@ void imprimeRangeP0(std::string &a) {
 
 void imprimeRangeP1(int &a) {
     cout << rangeTmp << " < " << a << "; " << rangeTmp << "++";
-    rangeTmp = "";
+    rangeTmp.clear();
+}
+
+void imprimeCompVar(std::string &a) {
+    cout << "COMP: " << a << "\n";
+    a.clear();
 }
 
 template <typename Iterator, typename Skipper>
@@ -85,7 +86,7 @@ class PyToCpp : public qi::grammar<Iterator, std::vector<std::string>(), Skipper
                     qi::char_(';'));
 
             /** for regra */
-            FOR_INSTRUCAO %=   qi::string("for")[&imprimeFor] >>
+            FOR_INSTRUCAO =   qi::string("for")[&imprimeFor] >>
                 qi::char_('(')[&imprimeChar] >>
                 RANGE_EXPRESSAO >> qi::char_(')')[&imprimeChar] >>
                 qi::char_('{')[&imprimeChar] >>
@@ -99,17 +100,22 @@ class PyToCpp : public qi::grammar<Iterator, std::vector<std::string>(), Skipper
             /** if regra */
             IF_INSTRUCAO %= qi::string("if")[&imprimeIf] >>
                 qi::char_('(')[&imprimeChar] >>
-                COMP_EXPRESSAO[imprimeAtrib] >> 
+                COMP_EXPRESSAO >>
                 qi::char_(')')[&imprimeChar] >>
                 qi::char_('{')[&imprimeChar] >>
                 *FOR_INSTRUCAO >> *IF_INSTRUCAO || *ATRIBUICAO >>
                 qi::char_('}')[&imprimeChar];
 
             /** comparacao regra */
-            COMP_EXPRESSAO = (VARIAVEL_INTEIRA >> lit('<') >> qi::int_) |
-                             (VARIAVEL_INTEIRA >> lit('>') >> qi::int_) |
-                             (VARIAVEL_INTEIRA >> qi::string("==") >> 
-                              qi::int_);
+            COMP_EXPRESSAO = (VARIAVEL_INTEIRA[&imprimeCompVar] >>
+                    qi::char_('<')[&imprimeChar] >>
+                    qi::int_[&imprimeInt]) |
+                    (VARIAVEL_INTEIRA[&imprimeCompVar] >>
+                    qi::char_('>')[&imprimeChar] >>
+                    qi::int_[&imprimeInt]) |
+                    (VARIAVEL_INTEIRA[&imprimeCompVar] >>
+                    qi::string("==")[&imprimeCompVar]  >> 
+                    qi::int_[&imprimeInt]);
 
             CODIGO = *ATRIBUICAO[&imprimeVar] ||
                      (*FOR_INSTRUCAO || *IF_INSTRUCAO) ||
